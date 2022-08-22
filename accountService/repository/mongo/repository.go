66d2b83d736dfabd3252784429ecec95a94c65cf
Kenta420 <poomipat002@gmail.com>
2022-08-22 14:28:14 +0700

    Implement method mongoDB repository

--- accountService/repository/mongo/repository.go
+++ accountService/repository/mongo/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/ce-final-project/backend_rest_api/accountService/core"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -43,18 +44,63 @@ func NewMongoRepository(mongoURL string, mongoDB string, mongoTimeout int) (core
 	return repo, nil
 }
 
-// TODO: Implement all this method
-func (m mongoRepository) Find(playerId string, accountId string) (*core.Account, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *mongoRepository) Find(playerId string, accountId string) (*core.Account, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), m.timeout)
+	defer cancel()
+	account := &core.Account{}
+	var filter bson.M
+	if playerId != "" {
+		filter["player_id"] = playerId
+	}
+	if accountId != "" {
+		filter["_id"] = accountId
+	}
+	collection := m.client.Database(m.database).Collection("accounts")
+	err := collection.FindOne(ctx, filter).Decode(&account)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("%v repository.Account.Find", core.ErrAccountNotFound)
+		}
+		return nil, fmt.Errorf("%v repository.Account.Find", err)
+	}
+	return account, nil
 }
 
-func (m mongoRepository) Store(account *core.Account) error {
-	//TODO implement me
-	panic("implement me")
+func (m *mongoRepository) Store(account *core.Account) error {
+	ctx, cancel := context.WithTimeout(context.Background(), m.timeout)
+	defer cancel()
+	collection := m.client.Database(m.database).Collection("accounts")
+	_, err := collection.InsertOne(ctx, bson.M{
+		"player_id":     account.PlayerId,
+		"username":      account.Username,
+		"email":         account.Email,
+		"password_hash": account.PasswordHash,
+		"created_at":    account.CreatedAt,
+	})
+	if err != nil {
+		return fmt.Errorf("%v repository.Account.Store", err)
+	}
+	return nil
 }
 
-func (m mongoRepository) Remove(playerId string, accountId string) (*core.Account, error) {
-	//TODO implement me
-	panic("implement me")
+func (m *mongoRepository) Remove(playerId string, accountId string) (*core.Account, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), m.timeout)
+	defer cancel()
+	account := &core.Account{}
+	var filter bson.M
+	if playerId != "" {
+		filter["player_id"] = playerId
+	}
+	if accountId != "" {
+		filter["_id"] = accountId
+	}
+	collection := m.client.Database(m.database).Collection("accounts")
+	err := collection.FindOneAndDelete(ctx, filter).Decode(&account)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, fmt.Errorf("%v repository.Account.Remove", core.ErrAccountNotFound)
+		}
+		return nil, fmt.Errorf("%v repository.Account.Remove", err)
+	}
+	return account, nil
 }
